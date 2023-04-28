f9240948eac037f8fab0158a754184553dc21918
alex <alexvetrici@gmail.com>
2023-04-28 08:44:51 +0200

    added some get request

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 const express = require('express')
 const app = express()
+
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Home page</h1>')
+})
+
+app.get('/contact',(req,res)=>{
+    res.send('<h1>Contact page</h1><div></div><label>example@gmail.com</label>')
+})
+
+app.get('/express',(req,res)=>{
+    res.redirect('https://expressjs.com')
+})
+
+app.get('/nodemon',(req,res)=>{
+    res.redirect('https://nodemon.io')
+})
+
+app.listen('3000')
